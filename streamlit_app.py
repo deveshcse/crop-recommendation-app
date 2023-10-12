a59f00e2e0e716e5325c23f7c1ac3db41044991a
devesh mishra <d.mishra56603@gmail.com>
2023-10-12 19:37:56 +0530

    completed the recommendation system

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+
 import pickle
 import warnings
 warnings.filterwarnings('ignore')
@@ -22,12 +22,16 @@ rainfall = st.number_input('Enter the value of rainfall')
 features = [n, p, k, temperature, humidity, ph, rainfall]
 feature_array = np.asarray(features)
 feature_array2d = feature_array.reshape(1,-1)
-# scaler = StandardScaler()
-# scaled_features = scaler.fit_transform([features])
-# crop = loaded_model.predict(feature_array2d)
-result = ''
+crops = ['Apple', 'Banana', 'Blackgram', 'Chickpea', 'Coconut', 'Coffee',
+       'Cotton', 'Grapes', 'Jute', 'Kidneybeans', 'Lentil', 'Maize',
+       'Mango', 'Mothbeans', 'Mungbean', 'Muskmelon', 'Orange', 'Papaya',
+       'Pigeonpeas', 'Pomegranate', 'Rice', 'Watermelon']
+
+labels = [0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+
 if st.button("Check the Result"):
     crop = loaded_model.predict(feature_array2d)
-    st.write(crop)
+    st.write("The most suitable crop for given soil:", crops[crop[0]])
+    st.write(crops[crop[0]])
 
 
